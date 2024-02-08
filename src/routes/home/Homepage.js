13eecb53873a385d433038c881fbecaaa4cf3ef8
Domenico <135279072+DevDomenico@users.noreply.github.com>
2024-02-08 21:50:33 +0100

    esercizio

--- src/routes/home/Homepage.js
+++ src/routes/home/Homepage.js
@@ -9,17 +9,26 @@
       {
         id: "1",
         place: "london",
-        image: "https://placekitten.com/300/200"
+        description: "A vibrant city with rich history and culture",
+        price: "$100 per night",
+        image: "https://placekitten.com/300/200",
+        category: "city"
       },
       {
         id: "2",
         place: "russia",
-        image: "https://placekitten.com/300/200"
+        description: "Explore the largest country in the world",
+        price: "$80 per night",
+        image: "https://placekitten.com/300/200",
+        category: "country"
       },
       {
         id: "3",
         place: "spain",
-        image: "https://placekitten.com/300/200"
+        description: "Experience the sunny beaches and delicious cuisine",
+        price: "$120 per night",
+        image: "https://placekitten.com/300/200",
+        category: "spiaggia"
       },
       // ... other places
     ];
@@ -40,18 +49,15 @@
     return (
       <div>
         <Navbars />
-        <div className="homepage">
-          <input
-            type="text"
-            value={inputValue}
-            onChange={handleInputChange}
-            placeholder="Search"
-          />
-          <div className="image-container">
+        <div className=" m-2 mt-5 homepage">
+         
+          <div className="d-flex mt-4 justify-content-center">
             {filteredPlaces.map((item) => (
-              <div className="card" key={item.id} style={{ display: "inline-block" }}>
-                <img src={item.image} alt={item.place} width={"200px"} />
-                <p>{item.place}</p>
+              <div className= "m-3 border-0  mt-4 card" key={item.id} style={{ display: "inline-block" }}>
+                <img className="rounded" src={item.image} alt={item.place} width={"250px"}  height={"250px"}/>
+                <p className="h3 px-2 m-1 font-weight-bold">{item.place}</p>
+                <p className="px-2 pt-1 mb-0 text-secondary">{item.category}</p>
+                <p className="px-2 h6">{item.price}</p>
               </div>
             ))}
           </div>
